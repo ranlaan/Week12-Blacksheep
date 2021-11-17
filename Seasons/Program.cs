198af8e8d0cc627a3b3dd4af074c7a79e68ce370
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:18:15 +0200

    master

---
+++ Seasons/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Seasons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Winterfile = @"C:\Users\opilane\Samples\winter.txt";
+            string Autumnfile = @"C:\Users\opilane\Samples\autumn.txt";
+
+            string[] winterData = File.ReadAllLines(Autumnfile);
+            string[] autumnData = File.ReadAllLines(Winterfile);
+
+            File.WriteAllLines(Autumnfile, autumnData);
+            File.WriteAllLines(Winterfile, winterData);
+        }
+    }
+}
